f4b68675c6314a4fd2015219f2cbc345505b81ff
Michael Truog <mjtruog@gmail.com>
2014-06-27 20:36:35 -0700

    Fix to match HTTP encoding.

--- src/cloudi_service.erl
+++ src/cloudi_service.erl
@@ -2364,13 +2364,12 @@ service_name_parse_with_suffix(Name, Pattern) ->
 %% @end
 %%-------------------------------------------------------------------------
 
--spec request_http_qs_parse(Request :: binary()) ->
+-spec request_http_qs_parse(Request :: binary() |
+                                       list({any(), any()})) ->
     Result :: dict().
 
-request_http_qs_parse(Request)
-    when is_binary(Request) ->
-    binary_key_value_parse_list(binary:split(Request, <<0>>, [global]),
-                                dict:new()).
+request_http_qs_parse(Request) ->
+    request_info_key_value_parse(Request).
 
 %%-------------------------------------------------------------------------
 %% @doc
