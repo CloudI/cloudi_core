2083cc39207ae13b74907587d6d11743fef2bafa
Michael Truog <mjtruog@gmail.com>
2014-07-23 19:41:26 -0700

    Fix unused function warnings.

--- src/cloudi_configuration.erl
+++ src/cloudi_configuration.erl
@@ -1322,8 +1322,9 @@ services_validate([#internal{
 -ifdef(CLOUDI_CORE_STANDALONE).
 -compile({nowarn_unused_function,
           [{services_validate_options_external, 2},
-           {services_validate_option_aspects_init_external, 2},
-           {services_validate_option_aspects_request_external, 2},
+           {services_validate_option_aspects_init_after_external, 2},
+           {services_validate_option_aspects_request_before_external, 2},
+           {services_validate_option_aspects_request_after_external, 2},
            {services_validate_option_aspects_external, 5}]}).
 -define(CLOUDI_CORE_SUPPORT_EXTERNAL,
     ;).
