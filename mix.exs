c6d63ab8ea6dd4b19bc0f6956e6b9a97f773f7e3
Michael Truog <mjtruog@gmail.com>
2016-09-14 16:26:13 -0700

    Version 1.5.3-rc1.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CloudICore.Mixfile do
 
   def project do
     [app: :cloudi_core,
-     version: "1.5.1-package",
+     version: "1.5.3-rc1",
      language: :erlang,
      description: description,
      package: package,
@@ -43,21 +43,20 @@ defmodule CloudICore.Mixfile do
   end
 
   defp deps do
-    [{:cpg, "~> 1.5.1"},
-     {:uuid, "~> 1.5.1", hex: :uuid_erl},
-     {:reltool_util, "~> 1.5.1"},
-     {:trie, "~> 1.5.1"},
-     {:erlang_term, "~> 1.5.1"},
-     {:quickrand, "~> 1.5.1"},
-     {:pqueue, "~> 1.5.1"},
-     {:key2value, "~> 1.5.1"},
-     {:keys1value, "~> 1.5.1"},
-     {:nodefinder, "~> 1.5.1"},
-     {:supool, "~> 1.5.1"},
-     {:varpool, "~> 1.5.1"},
-     {:dynamic_compile, "~> 1.0.0"},
+    [{:cpg, "~> 1.5.2"},
+     {:uuid, "~> 1.5.2", hex: :uuid_erl},
+     {:reltool_util, "~> 1.5.2"},
+     {:trie, "~> 1.5.2"},
+     {:erlang_term, "~> 1.5.2"},
+     {:quickrand, "~> 1.5.2"},
+     {:pqueue, "~> 1.5.2"},
+     {:key2value, "~> 1.5.2"},
+     {:keys1value, "~> 1.5.2"},
+     {:nodefinder, "~> 1.5.3"},
+     {:supool, "~> 1.5.2"},
+     {:varpool, "~> 1.5.2"},
      {:syslog, "~> 1.0.2"},
-     {:msgpack, "~> 0.3.5"}]
+     {:msgpack, "~> 0.6.0"}]
   end
 
   defp description do
