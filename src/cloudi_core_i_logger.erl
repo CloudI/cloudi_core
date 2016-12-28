df391fa4d96e3b495aa063548756881939c355eb
Michael Truog <mjtruog@gmail.com>
2016-12-27 20:58:51 -0800

    Make syslog defaults explicit.

--- src/cloudi_core_i_logger.erl
+++ src/cloudi_core_i_logger.erl
@@ -1748,9 +1748,11 @@ syslog_open(#config_logging_syslog{identity = SyslogIdentity,
                {transport, SyslogTransport},
                {transport_options, SyslogTransportOptions},
                {protocol, SyslogProtocol},
+               {utf8, true},
                {path, SyslogPath},
                {host, SyslogHost},
-               {port, SyslogPort}],
+               {port, SyslogPort},
+               {timeout, 5000}],
     {ok, Syslog} = syslog_socket:start_link(Options),
     Syslog.
 
