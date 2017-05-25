c652191d37186040e8c2284a7663f6db6e3b157f
Michael Truog <mjtruog@gmail.com>
2017-05-24 18:18:18 -0700

    Remove older compatibility code for maps/dict/queue.

--- src/cloudi_core_i_services_monitor.erl
+++ src/cloudi_core_i_services_monitor.erl
@@ -120,10 +120,9 @@
             key2value:
             key2value(cloudi_service_api:service_id(),
                                pid(), #service{}),
-        changes = maps:new() ::
-            maps_proxy(cloudi_service_api:service_id(),
-                       list({increase | decrease,
-                             number(), number(), number()}))
+        changes = #{} ::
+            #{cloudi_service_api:service_id() :=
+              list({increase | decrease, number(), number(), number()})}
     }).
 
 %%%------------------------------------------------------------------------
