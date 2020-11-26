1ecbf7112a834110ce7f77324daecb790c144d38
Michael Truog <mjtruog@protonmail.com>
2020-11-26 15:59:23 -0800

    Version 2.0.1.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudICore.Mixfile do
 
   def project do
     [app: :cloudi_core,
-     version: "2.0.0",
+     version: "2.0.1",
      language: :erlang,
      erlc_options: [
        {:d, :erlang.list_to_atom('ERLANG_OTP_VERSION_' ++ :erlang.system_info(:otp_release))},
@@ -61,19 +61,19 @@ defmodule CloudICore.Mixfile do
   end
 
   defp deps do
-    [{:cpg, "~> 2.0.0"},
-     {:uuid, "~> 2.0.0", hex: :uuid_erl},
-     {:reltool_util, "~> 2.0.0"},
-     {:trie, "~> 2.0.0"},
-     {:erlang_term, "~> 2.0.0"},
-     {:quickrand, "~> 2.0.0"},
-     {:pqueue, "~> 2.0.0"},
-     {:key2value, "~> 2.0.0"},
-     {:keys1value, "~> 2.0.0"},
-     {:nodefinder, "~> 2.0.0"},
-     {:supool, "~> 2.0.0"},
-     {:varpool, "~> 2.0.0"},
-     {:syslog_socket, "~> 2.0.0"}]
+    [{:cpg, "~> 2.0.1"},
+     {:uuid, "~> 2.0.1", hex: :uuid_erl},
+     {:reltool_util, "~> 2.0.1"},
+     {:trie, "~> 2.0.1"},
+     {:erlang_term, "~> 2.0.1"},
+     {:quickrand, "~> 2.0.1"},
+     {:pqueue, "~> 2.0.1"},
+     {:key2value, "~> 2.0.1"},
+     {:keys1value, "~> 2.0.1"},
+     {:nodefinder, "~> 2.0.1"},
+     {:supool, "~> 2.0.1"},
+     {:varpool, "~> 2.0.1"},
+     {:syslog_socket, "~> 2.0.1"}]
   end
 
   defp description do
